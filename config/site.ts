33d16425169975fade5d1cf8210e4efe8907c000
kais.radwan.personal <kais.radwan.personal@gmail.com>
2024-11-03 15:11:37 +0000

    we're good

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig: SiteConfig = {
   name: "kais_radwan",
   description:
     "Software developer building cool projects -> AI | UI/UX | BACK_END | FRONT_END | NEXT_JS",
-  url: "https://docs.scoopika.com",
-  ogImage: "https://docs.scoopika.com/og.png",
+  url: "https://kaisrad.vercel.app",
+  ogImage: "/og.png",
   links: {
     twitter: "https://x.com/kais_rad",
     github: "https://github.com/kais-radwan",
